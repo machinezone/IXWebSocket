70ef77a5d520eea3103c14641c8300f892e061bf
Benjamin Sergeant <bsergean@gmail.com>
2019-01-01 14:29:57 -0800

    (nitpick) reformat

--- examples/echo_server/echo_server.cpp
+++ examples/echo_server/echo_server.cpp
@@ -46,7 +46,6 @@ int main(int argc, char** argv)
                     }
                     else if (messageType == ix::WebSocket_MessageType_Message)
                     {
-
                         for (auto&& client : server.getClients())
                         {
                             if (client != webSocket)
