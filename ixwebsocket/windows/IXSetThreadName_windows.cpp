3f5935a284f0a455868a2b7607fe3658f96b3b5f
Benjamin Sergeant <bsergean@gmail.com>
2019-01-04 15:23:57 -0800

    windows fixes

---
+++ ixwebsocket/windows/IXSetThreadName_windows.cpp
@@ -0,0 +1,16 @@
+/*
+ *  IXSetThreadName_windows.cpp
+ *  Author: Benjamin Sergeant
+ *  Copyright (c) 2019 Machine Zone, Inc. All rights reserved.
+ */
+#include "../IXSetThreadName.h"
+#include <iostream>
+
+namespace ix
+{
+    void setThreadName(const std::string& name)
+    {
+        // FIXME
+        std::cerr << "setThreadName not implemented on Windows yet" << std::endl;
+    }
+}
