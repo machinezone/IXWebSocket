d00960b33f3c50d3e5f6848d73585b5069edb318
Benjamin Sergeant <bsergean@gmail.com>
2019-01-07 20:47:25 -0800

    Revert "try asan on Linux"
    This reverts commit dd2360ed7049e7be3cd9c739783ae744df62df2f.

--- test/run.py
+++ test/run.py
@@ -29,9 +29,6 @@ sanitizersFlags = {
     'none': ''
 }
 sanitizer = 'tsan'
-if osName == 'Linux':
-    sanitizer = 'asan'
-
 sanitizerFlags = sanitizersFlags[sanitizer]
 
 cmakeCmd = 'cmake -DCMAKE_BUILD_TYPE=Debug {} {} ..'.format(generator, sanitizerFlags)
