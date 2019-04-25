9936260711a489cb3ed74650cd48b20bd3f854d7
Benjamin Sergeant <bsergean@gmail.com>
2019-04-25 16:38:15 -0700

    initialize netSystem (aka winsock on windows) explicitely

--- test/test_runner.cpp
+++ test/test_runner.cpp
@@ -7,8 +7,14 @@
 #define CATCH_CONFIG_RUNNER
 #include "catch.hpp"
 
+#include <ixwebsocket/IXNetSystem.h>
+
 int main(int argc, char* argv[])
 {
+    ix::initNetSystem();
+
     int result = Catch::Session().run(argc, argv);
+
+    ix::uninitNetSystem();
     return result;
 }
