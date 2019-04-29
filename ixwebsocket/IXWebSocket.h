35203c460d08e0e6cda00f6595531db668e07bc9
Alexandre Konieczny <alexandrekonieczny@iMac-de-Alexandre.local>
2019-04-29 16:18:22 +0200

    fix data race on _thread

--- ixwebsocket/IXWebSocket.h
+++ ixwebsocket/IXWebSocket.h
@@ -156,6 +156,7 @@ namespace ix
         static OnTrafficTrackerCallback _onTrafficTrackerCallback;
 
         std::atomic<bool> _stop;
+        std::atomic<bool> _blocking;
         std::atomic<bool> _automaticReconnection;
         std::thread _thread;
         std::mutex _writeMutex;
