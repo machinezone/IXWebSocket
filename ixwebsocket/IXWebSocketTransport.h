243f41bf28d0050b0a15b67873e2debd8e9d5fb0
Benjamin Sergeant <bsergean@gmail.com>
2019-05-15 19:19:13 -0700

    use a regular mutex instead of a recursive one + stop properly

--- ixwebsocket/IXWebSocketTransport.h
+++ ixwebsocket/IXWebSocketTransport.h
@@ -154,7 +154,7 @@ namespace ix
 
         // Underlying TCP socket
         std::shared_ptr<Socket> _socket;
-        std::recursive_mutex _socketMutex;
+        std::mutex _socketMutex;
 
         // Hold the state of the connection (OPEN, CLOSED, etc...)
         std::atomic<ReadyState> _readyState;
