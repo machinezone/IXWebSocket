23cf4bd59b4ca17cb368d77ff05702a6f005a58b
Benjamin Sergeant <bsergean@gmail.com>
2019-06-09 10:10:33 -0700

    Add explicite WebSocket::sendBinary New headers + WebSocketMessage class to hold message data, still not used across the board

--- ixwebsocket/IXWebSocketMessageQueue.h
+++ ixwebsocket/IXWebSocketMessageQueue.h
@@ -30,24 +30,12 @@ namespace ix
         void poll(int count = 512);
 
     protected:
-        struct Message
-        {
-            WebSocketMessageType type;
-            std::string str;
-            size_t wireSize;
-            WebSocketErrorInfo errorInfo;
-            WebSocketOpenInfo openInfo;
-            WebSocketCloseInfo closeInfo;
-        };
-
-        using MessagePtr = std::shared_ptr<Message>;
-
-        MessagePtr popMessage();
+        WebSocketMessagePtr popMessage();
 
     private:
         WebSocket* _websocket = nullptr;
         OnMessageCallback _onMessageUserCallback;
         std::mutex _messagesMutex;
-        std::list<MessagePtr> _messages;
+        std::list<WebSocketMessagePtr> _messages;
     };
 } // namespace ix
