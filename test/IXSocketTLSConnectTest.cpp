cf7d591667256b242e5464e8053f47b204c47ca1
Matt DeBoer <mdeboer@soundwave.cilab>
2019-09-23 13:49:37 -0700

    add some additional tests

--- test/IXSocketTLSConnectTest.cpp
+++ test/IXSocketTLSConnectTest.cpp
@@ -19,18 +19,13 @@ TEST_CASE("tls_socket_connect", "[net]")
     SECTION("Test connecting with a trusted cert")
     {
         int port = getFreePort();
-        ix::WebSocketServer server(port,
-                                   SocketServer::kDefaultHost,
-                                   SocketServer::kDefaultTcpBacklog,
-                                   SocketServer::kDefaultMaxConnections,
-                                   60);
+        ix::WebSocketServer server(port);
         server.setTLSOptions({".certs/trusted-server-crt.pem",
                               ".certs/trusted-server-key.pem",
                               ".certs/trusted-ca-crt.pem"});
         REQUIRE(startWebSocketEchoServer(server));
 
         std::cerr << "server listening on port " << port << std::endl;
-        // std::this_thread::sleep_for(std::chrono::minutes(300));
 
         ix::WebSocket client;
         auto url = "wss://localhost:" + std::to_string(port) + "/";
@@ -65,38 +60,196 @@ TEST_CASE("tls_socket_connect", "[net]")
         REQUIRE(status.success);
     }
 
-    // SECTION("Test connecting with an untrusted cert")
-    // {
-    //     std::string errMsg;
-    //     std::string hostname("12313lhjlkjhopiupoijlkasdckljqwehrlkqjwehraospidcuaposidcasdc");
-    //     int fd = SocketConnect::connect(hostname, 80, errMsg, [] { return false; });
-    //     std::cerr << "Error message: " << errMsg << std::endl;
-    //     REQUIRE(fd == -1);
-    // }
+    SECTION("Test connecting with an untrusted cert")
+    {
+        int port = getFreePort();
+        ix::WebSocketServer server(port);
+        server.setTLSOptions({".certs/trusted-server-crt.pem",
+                              ".certs/trusted-server-key.pem",
+                              ".certs/trusted-ca-crt.pem"});
+        REQUIRE(startWebSocketEchoServer(server));
 
-    // SECTION("Test connecting with a trusted self-signed cert")
-    // {
-    //     int port = getFreePort();
-    //     ix::WebSocketServer server(port);
-    //     REQUIRE(startWebSocketEchoServer(server));
+        std::cerr << "server listening on port " << port << std::endl;
 
-    //     std::string errMsg;
-    //     // The callback returning true means we are requesting cancellation
-    //     int fd = SocketConnect::connect("127.0.0.1", port, errMsg, [] { return true; });
-    //     std::cerr << "Error message: " << errMsg << std::endl;
-    //     REQUIRE(fd == -1);
-    // }
+        ix::WebSocket client;
+        auto url = "wss://localhost:" + std::to_string(port) + "/";
+        
+        ix::WebSocketErrorInfo errInfo;
+        client.setUrl(url);
+        client.setTLSOptions({".certs/untrusted-client-crt.pem",
+                              ".certs/untrusted-client-key.pem",
+                              ".certs/trusted-ca-crt.pem"});
+        client.setOnMessageCallback([&errInfo](const ix::WebSocketMessagePtr& msg) {
+            if (msg->type == ix::WebSocketMessageType::Message)
+            {
+                std::cerr << "Client received: " << msg->str << std::endl;
+            }
+            else if (msg->type == ix::WebSocketMessageType::Error)
+            {
+                std::stringstream ss;
+                ss << "Error: " << msg->errorInfo.reason << std::endl;
+                ss << "#retries: " << msg->errorInfo.retries << std::endl;
+                ss << "Wait time(ms): " << msg->errorInfo.wait_time << std::endl;
+                ss << "HTTP Status: " << msg->errorInfo.http_status << std::endl;
+                std::cerr << ss.str() << std::endl;
+                errInfo = msg->errorInfo;
+            }
+        });
+        client.start();
+        auto timed_out = std::chrono::system_clock::now() + std::chrono::seconds(2);
+        while (client.getReadyState() != ix::ReadyState::Open &&
+               std::chrono::system_clock::now() < timed_out)
+        {
+            std::this_thread::sleep_for(std::chrono::milliseconds(10));
+        }
+        auto status = client.sendText("Hello");
+        client.stop();
+        REQUIRE(status.success == false);
+        
+        using Catch::Matchers::Contains;
+        REQUIRE_THAT(errInfo.reason, Contains("tlsv1 alert unknown ca"));
+    }
 
-    // SECTION("Test connecting with an untrusted self-signed cert")
-    // {
-    //     int port = getFreePort();
-    //     ix::WebSocketServer server(port);
-    //     REQUIRE(startWebSocketEchoServer(server));
+    SECTION("Test connecting with a trusted self-signed cert")
+    {
+        int port = getFreePort();
+        ix::WebSocketServer server(port);
+        server.setTLSOptions({".certs/trusted-server-crt.pem",
+                              ".certs/trusted-server-key.pem",
+                              ".certs/selfsigned-client-crt.pem"});
+        REQUIRE(startWebSocketEchoServer(server));
 
-    //     std::string errMsg;
-    //     // The callback returning true means we are requesting cancellation
-    //     int fd = SocketConnect::connect("127.0.0.1", port, errMsg, [] { return true; });
-    //     std::cerr << "Error message: " << errMsg << std::endl;
-    //     REQUIRE(fd == -1);
-    // }
+        std::cerr << "server listening on port " << port << std::endl;
+
+        ix::WebSocket client;
+        auto url = "wss://localhost:" + std::to_string(port) + "/";
+        
+        ix::WebSocketErrorInfo errInfo;
+        client.setUrl(url);
+        client.setTLSOptions({".certs/selfsigned-client-crt.pem",
+                              ".certs/selfsigned-client-key.pem",
+                              ".certs/trusted-ca-crt.pem"});
+        client.setOnMessageCallback([&errInfo](const ix::WebSocketMessagePtr& msg) {
+            if (msg->type == ix::WebSocketMessageType::Message)
+            {
+                std::cerr << "Client received: " << msg->str << std::endl;
+            }
+            else if (msg->type == ix::WebSocketMessageType::Error)
+            {
+                std::stringstream ss;
+                ss << "Error: " << msg->errorInfo.reason << std::endl;
+                ss << "#retries: " << msg->errorInfo.retries << std::endl;
+                ss << "Wait time(ms): " << msg->errorInfo.wait_time << std::endl;
+                ss << "HTTP Status: " << msg->errorInfo.http_status << std::endl;
+                std::cerr << ss.str() << std::endl;
+                errInfo = msg->errorInfo;
+            }
+        });
+        client.start();
+        auto timed_out = std::chrono::system_clock::now() + std::chrono::seconds(2);
+        while (client.getReadyState() != ix::ReadyState::Open &&
+               std::chrono::system_clock::now() < timed_out)
+        {
+            std::this_thread::sleep_for(std::chrono::milliseconds(10));
+        }
+        auto status = client.sendText("Hello");
+        client.stop();
+        REQUIRE(status.success);
+    }
+
+    SECTION("Test connecting with a trusted self-signed cert, no client verification")
+    {
+        int port = getFreePort();
+        ix::WebSocketServer server(port);
+        server.setTLSOptions({".certs/trusted-server-crt.pem",
+                              ".certs/trusted-server-key.pem",
+                              ".certs/selfsigned-client-crt.pem"});
+        REQUIRE(startWebSocketEchoServer(server));
+
+        std::cerr << "server listening on port " << port << std::endl;
+
+        ix::WebSocket client;
+        auto url = "wss://localhost:" + std::to_string(port) + "/";
+        
+        ix::WebSocketErrorInfo errInfo;
+        client.setUrl(url);
+        client.setTLSOptions({".certs/selfsigned-client-crt.pem",
+                              ".certs/selfsigned-client-key.pem",
+                              "NONE"});
+        client.setOnMessageCallback([&errInfo](const ix::WebSocketMessagePtr& msg) {
+            if (msg->type == ix::WebSocketMessageType::Message)
+            {
+                std::cerr << "Client received: " << msg->str << std::endl;
+            }
+            else if (msg->type == ix::WebSocketMessageType::Error)
+            {
+                std::stringstream ss;
+                ss << "Error: " << msg->errorInfo.reason << std::endl;
+                ss << "#retries: " << msg->errorInfo.retries << std::endl;
+                ss << "Wait time(ms): " << msg->errorInfo.wait_time << std::endl;
+                ss << "HTTP Status: " << msg->errorInfo.http_status << std::endl;
+                std::cerr << ss.str() << std::endl;
+                errInfo = msg->errorInfo;
+            }
+        });
+        client.start();
+        auto timed_out = std::chrono::system_clock::now() + std::chrono::seconds(2);
+        while (client.getReadyState() != ix::ReadyState::Open &&
+               std::chrono::system_clock::now() < timed_out)
+        {
+            std::this_thread::sleep_for(std::chrono::milliseconds(10));
+        }
+        auto status = client.sendText("Hello");
+        client.stop();
+        REQUIRE(status.success);
+    }
+
+    SECTION("client uses trusted self-signed cert, untrusted server, no client verification, ALL ciphers")
+    {
+        int port = getFreePort();
+        ix::WebSocketServer server(port);
+        server.setTLSOptions({".certs/trusted-server-crt.pem",
+                              ".certs/trusted-server-key.pem",
+                              ".certs/selfsigned-client-crt.pem"});
+        REQUIRE(startWebSocketEchoServer(server));
+
+        std::cerr << "server listening on port " << port << std::endl;
+
+        ix::WebSocket client;
+        auto url = "wss://localhost:" + std::to_string(port) + "/";
+        
+        ix::WebSocketErrorInfo errInfo;
+        client.setUrl(url);
+        client.setTLSOptions({".certs/selfsigned-client-crt.pem",
+                              ".certs/selfsigned-client-key.pem",
+                              "NONE", // client doesn't verify server
+                              "ALL" // client is okay with any possible cipher
+                              });
+        client.setOnMessageCallback([&errInfo](const ix::WebSocketMessagePtr& msg) {
+            if (msg->type == ix::WebSocketMessageType::Message)
+            {
+                std::cerr << "Client received: " << msg->str << std::endl;
+            }
+            else if (msg->type == ix::WebSocketMessageType::Error)
+            {
+                std::stringstream ss;
+                ss << "Error: " << msg->errorInfo.reason << std::endl;
+                ss << "#retries: " << msg->errorInfo.retries << std::endl;
+                ss << "Wait time(ms): " << msg->errorInfo.wait_time << std::endl;
+                ss << "HTTP Status: " << msg->errorInfo.http_status << std::endl;
+                std::cerr << ss.str() << std::endl;
+                errInfo = msg->errorInfo;
+            }
+        });
+        client.start();
+        auto timed_out = std::chrono::system_clock::now() + std::chrono::seconds(2);
+        while (client.getReadyState() != ix::ReadyState::Open &&
+               std::chrono::system_clock::now() < timed_out)
+        {
+            std::this_thread::sleep_for(std::chrono::milliseconds(10));
+        }
+        auto status = client.sendText("Hello");
+        client.stop();
+        REQUIRE(status.success);
+    }
 }
