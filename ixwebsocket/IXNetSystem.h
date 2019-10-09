11a3b6465764b7f4b552fa148e417acda4ab35b7
Benjamin Sergeant <bsergean@gmail.com>
2019-10-09 15:38:40 -0700

    (freebsd compile fix) add some missing socket related headers

--- ixwebsocket/IXNetSystem.h
+++ ixwebsocket/IXNetSystem.h
@@ -21,6 +21,8 @@ typedef unsigned long int nfds_t;
 #include <errno.h>
 #include <netdb.h>
 #include <netinet/tcp.h>
+#include <netinet/in.h>
+#include <netinet/ip.h>
 #include <poll.h>
 #include <sys/select.h>
 #include <sys/socket.h>
