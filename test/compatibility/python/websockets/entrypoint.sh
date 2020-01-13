0c4866420a3d5d8eafd3b6f381ccf582f933752d
Benjamin Sergeant <bsergean@gmail.com>
2020-01-12 21:32:26 -0800

    test broadcast server

--- test/compatibility/python/websockets/entrypoint.sh
+++ test/compatibility/python/websockets/entrypoint.sh
@@ -7,4 +7,10 @@ case $MODE in
     echo_server_ssl)
         python /usr/bin/echo_server_ssl.py
         ;;
+    broadcast_server)
+        python /usr/bin/broadcast_server.py
+        ;;
+    broadcast_server_ssl)
+        python /usr/bin/broadcast_server_ssl.py
+        ;;
 esac
