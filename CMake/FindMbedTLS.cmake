1e166eee579c34d8a7d63252f6736a07c8a32925
Benjamin Sergeant <bsergean@gmail.com>
2021-10-22 10:57:51 -0700

    cmake change find header file

--- CMake/FindMbedTLS.cmake
+++ CMake/FindMbedTLS.cmake
@@ -1,5 +1,8 @@
 find_path(MBEDTLS_INCLUDE_DIRS mbedtls/ssl.h)
 
+# mbedtls-3.0 changed headers files, and we need to ifdef'out a few things
+find_path(MBEDTLS_VERSION_GREATER_THAN_3 mbedtls/build_info.h)
+
 find_library(MBEDTLS_LIBRARY mbedtls)
 find_library(MBEDX509_LIBRARY mbedx509)
 find_library(MBEDCRYPTO_LIBRARY mbedcrypto)
